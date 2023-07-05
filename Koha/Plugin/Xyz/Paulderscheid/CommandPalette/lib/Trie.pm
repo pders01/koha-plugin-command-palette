5d9f2514fe44b4112cdd0b86a28a28165a5a340d
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-07-05 18:07:20 +0200

    Attempt to improve maintainability by moving class methods to TrieNode

--- Koha/Plugin/Xyz/Paulderscheid/CommandPalette/lib/Trie.pm
+++ Koha/Plugin/Xyz/Paulderscheid/CommandPalette/lib/Trie.pm
@@ -4,27 +4,28 @@ use Modern::Perl;
 use utf8;
 use 5.032;
 
-use Moose;
+use Moo;
 use Koha::Plugin::Xyz::Paulderscheid::CommandPalette::lib::TrieNode;
 
 our $VERSION = '1.0.0';
 
 has 'root' => (
     is      => 'ro',
-    isa     => 'Koha::Plugin::Xyz::Paulderscheid::CommandPalette::lib::TrieNode',
-    default => sub { return Koha::Plugin::Xyz::Paulderscheid::CommandPalette::lib::TrieNode->new() },
+    default => sub { Koha::Plugin::Xyz::Paulderscheid::CommandPalette::lib::TrieNode->new },
 );
 
 sub insert {
     my ( $self, $path ) = @_;
     my @components = split /\//smx, $path;
     my $node       = $self->root;
+
     foreach my $component (@components) {
-        if ( !$node->children->{$component} ) {
-            $node->children->{$component} = Koha::Plugin::Xyz::Paulderscheid::CommandPalette::lib::TrieNode->new();
+        if ( !$node->has_child($component) ) {
+            $node->add_child($component);
         }
-        $node = $node->children->{$component};
+        $node = $node->get_child($component);
     }
+
     $node->is_end_of_path(1);
 
     return;
@@ -37,16 +38,14 @@ sub search {
     push @stack, [ $self->root, q{} ];
 
     while (@stack) {
-        my $node_path = pop @stack;
-        my $node      = $node_path->[0];
-        my $path      = $node_path->[1];
+        my ( $node, $path ) = @{ pop @stack };
 
-        if ( $node->is_end_of_path && $path =~ /$substring/smx ) {
+        if ( $node->is_end_of_path && index( $path, $substring ) >= 0 ) {
             push @matches, "/$path";
         }
 
-        foreach my $component ( keys %{ $node->children } ) {
-            my $child_node = $node->children->{$component};
+        foreach my $component ( $node->get_child_keys ) {
+            my $child_node = $node->get_child($component);
             my $child_path = $path eq q{} ? $component : "$path/$component";
 
             push @stack, [ $child_node, $child_path ];
@@ -59,12 +58,15 @@ sub search {
 sub collect_paths {
     my ( $self, $node ) = @_;
     my @paths;
+
     if ( $node->is_end_of_path ) {
-        push @paths, $node->path;    # Store the full path associated with the node
+        push @paths, $node->get_path;
     }
-    foreach my $child_node ( values %{ $node->children } ) {
+
+    foreach my $child_node ( $node->get_children ) {
         push @paths, $self->collect_paths($child_node);
     }
+
     return @paths;
 }
 
--- Koha/Plugin/Xyz/Paulderscheid/CommandPalette/lib/TrieNode.pm
+++ Koha/Plugin/Xyz/Paulderscheid/CommandPalette/lib/TrieNode.pm
@@ -3,21 +3,50 @@ package Koha::Plugin::Xyz::Paulderscheid::CommandPalette::lib::TrieNode;
 use Modern::Perl;
 use utf8;
 use 5.032;
+use Carp;
 
-use Moose;
+use Moo;
 
 our $VERSION = '1.0.0';
 
 has 'children' => (
     is      => 'ro',
-    isa     => 'HashRef[TrieNode]',
+    isa     => sub { croak 'Invalid children data type. Must be a HashRef.' if ( !( ref $_[0] eq 'HASH' ) ) },
     default => sub { return {} },
 );
 
 has 'is_end_of_path' => (
     is      => 'rw',
-    isa     => 'Bool',
+    isa     => sub { croak 'is_end_of_path must be a Boolean value.' if ( !( $_[0] == 0 || $_[0] == 1 ) ) },
     default => 0,
 );
+
+sub add_child {
+    my ( $self, $key ) = @_;
+    $self->children->{$key} = Koha::Plugin::Xyz::Paulderscheid::CommandPalette::lib::TrieNode->new;
+    return;
+}
+
+sub has_child {
+    my ( $self, $key ) = @_;
+    return exists $self->children->{$key};
+}
+
+sub get_child {
+    my ( $self, $key ) = @_;
+    return $self->children->{$key};
+}
+
+sub get_child_keys {
+    my ($self) = @_;
+    return keys %{ $self->children };
+}
+
+sub get_children {
+    my ($self) = @_;
+    return values %{ $self->children };
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
