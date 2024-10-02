c22bef3f131961e20a2334d67924c425da3e396e
pders01 <paulderscheid@gmail.com>
2024-10-02 20:33:42 +0200

    feat: add some color to update-meta's output

--- scripts/update-meta.sh
+++ scripts/update-meta.sh
@@ -1,14 +1,35 @@
 #!/usr/bin/env bash
 
-echo "Stashing local changes"
+if [[ -z "$NO_COLOR" ]]; then
+    CYAN='\033[0;36m'
+    RED='\033[0;31m'
+    NC='\033[0m' # No Color
+else
+    CYAN=''
+    RED=''
+    NC=''
+fi
+
+exit_on_failure() {
+    local message=$1
+    echo -e "${RED}${message}${NC}"
+    exit 1
+}
+
+# Stash local changes
+echo -e "${CYAN}Stashing local changes...${NC}"
 if ! git stash; then
-    exit 1
+    exit_on_failure "Failed to stash local changes."
 fi
 
-echo "Pulling latest commits from koha-plugin:main"
+# Pull latest changes from the repository
+echo -e "${CYAN}Pulling latest commits from koha-plugin:main...${NC}"
 if ! git pull https://github.com/pders01/koha-plugin.git main; then
-    exit 1
+    exit_on_failure "Failed to pull latest changes."
 fi
 
-echo "Popping stash"
-git stash pop
+# Pop the stash
+echo -e "${CYAN}Popping stash...${NC}"
+if ! git stash pop; then
+    exit_on_failure "Failed to pop the stash."
+fi
